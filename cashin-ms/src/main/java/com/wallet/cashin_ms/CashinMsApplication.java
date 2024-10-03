255626c8855dfe2a99d04d269887c006ed7a0e12
Rafael Lemes <rafael.sslemes@gmail.com>
2024-10-03 08:06:37 -0300

    initial arch of chashIn microservice

---
+++ cashin-ms/src/main/java/com/wallet/cashin_ms/CashinMsApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.cashin_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashinMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashinMsApplication.class, args);
+	}
+
+}
---
+++ cashin-ms/src/test/java/com/wallet/cashin_ms/CashinMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.cashin_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashinMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
