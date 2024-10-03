9baf966e3349a2794be97a346cd9b6d129f9da17
Rafael Lemes <rafael.sslemes@gmail.com>
2024-10-03 08:03:47 -0300

    initial arch external mocks

---
+++ external-mock/src/main/java/com/wallet/external_mock/ExternalMockApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.external_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalMockApplication.class, args);
+	}
+
+}
---
+++ external-mock/src/test/java/com/wallet/external_mock/ExternalMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.external_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
