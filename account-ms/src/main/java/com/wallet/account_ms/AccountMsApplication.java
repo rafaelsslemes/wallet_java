4387fe6e51c1552083b4e8e28656443ca4ef01bd
Rafael Lemes <rafael.sslemes@gmail.com>
2024-10-06 00:55:23 -0300

    add account-ms

---
+++ account-ms/src/main/java/com/wallet/account_ms/AccountMsApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.account_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMsApplication.class, args);
+	}
+
+}
