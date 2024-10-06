8cf2a16999de739565fc9af15f2200c06e5e2468
Rafael Lemes <rafael.sslemes@gmail.com>
2024-10-06 08:52:21 -0300

    add statement-ms

---
+++ statement-ms/src/main/java/com/wallet/statement_ms/StatementMsApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.statement_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatementMsApplication.class, args);
+	}
+
+}
